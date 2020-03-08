e38c93ff9c0c05fe689981a581f4a5ba1adb9bed
daldo <kimdohyunlll@naver.com>
2020-03-08 19:46:31 +0900

    1. Add function to Naver Login 2. Modify test to spring security

--- src/test/java/com/example/boot/web/HelloControllerTest.java
+++ src/test/java/com/example/boot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.boot.web;
 
+import com.example.boot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,12 +16,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void return_hello() throws Exception {
         String hello = "hello";
 
@@ -27,6 +32,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void return_helloDTO() throws Exception {
         String name = "hello";
         int amount = 1000;
