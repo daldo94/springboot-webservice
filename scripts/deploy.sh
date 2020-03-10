fa3cd9d58fef6cc4b19425043517d2f67d899d36
daldo <kimdohyunlll@naver.com>
2020-03-11 00:37:49 +0900

    Modify deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,11 +29,11 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-sudo chmod +x $JAR_NAME
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-sudo nohup java -jar \
+nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=real \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
